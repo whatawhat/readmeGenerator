4560cf47fd7366d5383ac4d7cb5aa5fa41b7b6b9
Stephanie <whatawhat@hotmail.com>
2021-05-19 19:42:24 -0400

    these are files needed; I didn't do anything to them

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,28 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+  ## ${data.description}
+  ## ${data.installation}
+  ## ${data.usage}
+  ## ${data.license}
+  ## ${data.contributing}
+  ## ${data.tests}
+  ## ${data.questions}
+  
+${renderLicenseBadge(data.license)}
+`;
+}
+
+module.exports = generateMarkdown;
